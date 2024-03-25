991837746fb7f224c253863ec5cc692269a67da2
SeeYouth <julienfroger16@gmail.com>
2024-03-25 14:57:05 +0100

    update navBar color svg and :hover

--- siteRWF/frontend/src/components/NavBar/NavBar.jsx
+++ siteRWF/frontend/src/components/NavBar/NavBar.jsx
@@ -3,8 +3,8 @@ import React from "react";
 import { FacebookLogo } from "../../assets/img/svg/logoSvg-facebook.jsx";
 import { LinkedInLogo } from "../../assets/img/svg/LinkedIn_icon.jsx";
 import { logoRwf } from "../logoRwf/logoRwf";
-import { LogoYt } from "../../assets/img/svg/IconYoutube.jsx";
 import { Xlogo } from "../../assets/img/svg/X_logo.jsx";
+import { YtLogo } from "../../assets/img/svg/YtLogo.jsx";
 
 const NavBar = () => {
   return (
@@ -12,12 +12,12 @@ const NavBar = () => {
       {logoRwf}
       <ul className="socialBar">
         <li>
-          <p>Vous avez un projet ?</p>
+          <p className="svgBtn">Vous avez un projet ?</p>
         </li>
         <li>{Xlogo}</li>
         <li>{FacebookLogo}</li>
         <li>{LinkedInLogo}</li>
-        <li>{LogoYt}</li>
+        <li>{YtLogo}</li>
       </ul>
       <ul>
         <li></li>
