de20d681b790e0598c87e301535c62c0b4a266fb
SeeYouth <julienfroger16@gmail.com>
2024-03-28 20:47:07 +0100

    update svg gear animation

--- siteRWF/frontend/src/pages/App.jsx
+++ siteRWF/frontend/src/pages/App.jsx
@@ -4,6 +4,7 @@ import "swiper/css";
 import React from "react";
 
 import { dataIcon } from "../assets/img/svg/dataIcon";
+import Gear from "../assets/img/svg/gear";
 import NavBar from "../components/NavBar";
 import Slider from "../components/Slider";
 
@@ -19,7 +20,7 @@ const App = () => {
             sur mesure et 100% français !
           </p>
         </section>
-        <section>
+        <section className="infoRwf">
           <h2>Notre expérience au service de vos projets web</h2>
           <ul className="skillContainer">
             <li className="skill">
@@ -30,9 +31,30 @@ const App = () => {
                 clients et concevoir des projets à fort trafic.
               </p>
             </li>
-            <li></li>
-            <li></li>
-            <li></li>
+            <li className="skill">
+              <Gear />
+              <h4>hébergement</h4>
+              <p>
+                Notre savoir-faire en hébergement nous permet de conseiller nos
+                clients et concevoir des projets à fort trafic.
+              </p>
+            </li>
+            <li className="skill">
+              {dataIcon}
+              <h4>hébergement</h4>
+              <p>
+                Notre savoir-faire en hébergement nous permet de conseiller nos
+                clients et concevoir des projets à fort trafic.
+              </p>
+            </li>
+            <li className="skill">
+              {dataIcon}
+              <h4>hébergement</h4>
+              <p>
+                Notre savoir-faire en hébergement nous permet de conseiller nos
+                clients et concevoir des projets à fort trafic.
+              </p>
+            </li>
           </ul>
         </section>
       </main>
