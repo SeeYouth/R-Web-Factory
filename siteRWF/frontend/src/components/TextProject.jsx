d71adbea9f4945e4bb26a06f12ed1bb69c88cac3
SeeYouth <julienfroger16@gmail.com>
2024-03-29 20:39:46 +0100

    add flag France

--- siteRWF/frontend/src/components/TextProject.jsx
+++ siteRWF/frontend/src/components/TextProject.jsx
@@ -1,9 +1,12 @@
 import React from "react";
 
+import SvgFlagFrance from "../assets/img/svg/SvgFlagFrance";
+
 const TextProject = () => {
   return (
     <section className="textProject">
       <p className="textProjets">vos projets sur mesure</p>
+      <SvgFlagFrance />
     </section>
   );
 };
