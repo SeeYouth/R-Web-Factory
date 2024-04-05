d657ecd39f4d074fe79a27984cc50e83d8e32627
SeeYouth <julienfroger16@gmail.com>
2024-04-05 16:25:06 +0200

    update style form not finish

--- siteRWF/frontend/src/components/FormContact.jsx
+++ siteRWF/frontend/src/components/FormContact.jsx
@@ -31,53 +31,61 @@ const FormContact = () => {
         C'est ici
       </h2>
       <form method="post">
-        {arrayInpText[0].map((company) => (
-          <div key={company.id}>{company.inputText}</div>
-        ))}
-        {arrayInpRadio[0].map((civility, index) => (
-          <div key={"civility" + civility + index}>
-            <label htmlFor={civility}>{civility}</label>
-            <input type="radio" name="civility" id={civility} />
-          </div>
-        ))}
-        <h3>Quel style de projet avez-vous imaginé ?</h3>
-        {category.map((item, index) => (
-          <div key={"InpRadio" + item + index}>
-            <label htmlFor={item}> {item} </label>
-            <input type="radio" name="category" id={item} />
-          </div>
-        ))}
-        <ReactSlider
-          className="horizontalSlider"
-          thumbClassName="budgetThumb"
-          trackClassName="budgetTrack"
-          defaultValue={value}
-          onChange={handleChange}
-          ariaLabel={["Lower thumb", "Upper thumb"]}
-          ariaValuetext={(state) => `Thumb value ${state.valueNow}`}
-          renderThumb={(props, state) => (
-            <div {...props}>{formatBudget(state.valueNow)}</div>
-          )}
-          pearling
-          step={100}
-          minDistance={2500}
-          min={500}
-          max={30000}
-        />
-        <h3>Avez-vous déjà votre cahier des charges ?</h3>
-        {arrayInpRadio[1].map((item, index) => (
-          <div key={"cdc" + item + index}>
-            <label htmlFor={item}>{item}</label>
-            <input type="radio" name="cdc" id={item} />
-          </div>
-        ))}
-        <textarea
-          name="explicationProject"
-          id="explicationProject"
-          cols="30"
-          rows="10"
-          placeholder="Expliquez-nous en quelques lignes votre rêve"
-        ></textarea>
+        <fieldset className="styleForm">
+          {arrayInpText[0].map((company) => (
+            <React.Fragment key={company.id}>
+              {company.inputText}
+            </React.Fragment>
+          ))}
+          {arrayInpRadio[0].map((civility, index) => (
+            <React.Fragment key={"civility" + civility + index}>
+              <input type="radio" name="civility" id={civility} />
+              <label htmlFor={civility}>{civility}</label>
+            </React.Fragment>
+          ))}
+        </fieldset>
+        <fieldset className="styleForm">
+          <h3>Quel style de projet avez-vous imaginé ?</h3>
+          {category.map((item, index) => (
+            <React.Fragment key={"InpRadio" + item + index}>
+              <input type="radio" name="category" id={item} />
+              <label htmlFor={item}> {item} </label>
+            </React.Fragment>
+          ))}
+          <ReactSlider
+            className="horizontalSlider"
+            thumbClassName="budgetThumb"
+            trackClassName="budgetTrack"
+            defaultValue={value}
+            onChange={handleChange}
+            ariaLabel={["Lower thumb", "Upper thumb"]}
+            ariaValuetext={(state) => `Thumb value ${state.valueNow}`}
+            renderThumb={(props, state) => (
+              <div {...props}>{formatBudget(state.valueNow)}</div>
+            )}
+            pearling
+            step={100}
+            minDistance={2500}
+            min={500}
+            max={30000}
+          />
+        </fieldset>
+        <fieldset className="styleForm">
+          <h3>Avez-vous déjà votre cahier des charges ?</h3>
+          {arrayInpRadio[1].map((item, index) => (
+            <React.Fragment key={"cdc" + item + index}>
+              <input type="radio" name="cdc" id={item} />
+              <label htmlFor={item}>{item}</label>
+            </React.Fragment>
+          ))}
+          <textarea
+            name="explicationProject"
+            id="explicationProject"
+            cols="30"
+            rows="10"
+            placeholder="Expliquez-nous en quelques lignes votre rêve"
+          ></textarea>
+        </fieldset>
       </form>
     </main>
   );
